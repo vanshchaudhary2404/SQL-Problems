49e97ce61bc8f7d248f8cf4765707fd779022f7d
Vansh Kumar <vanshchaudhary2404@gmail.com>
2025-12-18 00:03:51 +0530

    Update and rename Recyclable and Low Fat Products to Recyclable and Low Fat Products.sql

---
+++ Recyclable and Low Fat Products.sql
@@ -0,0 +1,10 @@
+/* Author : Vansh Kumar
+  Date : 2025-12-18
+  Description: Problem 1
+*/
+
+-- Write your MySQL query statement below
+
+SELECT product_id
+FROM Products 
+WHERE low_fats = 'Y' AND recyclable = 'Y';
