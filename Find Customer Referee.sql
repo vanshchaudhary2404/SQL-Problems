0335e64485d45685ff673f9b9ccea32cc646f805
Vansh Kumar <vanshchaudhary2404@gmail.com>
2025-12-19 01:36:48 +0530

    Create Find Customer Referee.sql

---
+++ Find Customer Referee.sql
@@ -0,0 +1,8 @@
+/* Author : Vansh Kumar
+  Date : 2025-12-19
+  Description: Leetcode problem-2
+*/
+-- # Write your MySQL query statement below
+SELECT name
+FROM Customer
+WHERE referee_id != 2 OR referee_id is null ;
