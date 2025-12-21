cf22318a7c7dde11586be798c583eeea6fa7125c
Vansh Kumar <vanshchaudhary2404@gmail.com>
2025-12-21 21:39:21 +0530

    Update Replace Employee ID with the unique identifier.sql

--- Replace Employee ID with the unique identifier.sql
+++ Replace Employee ID with the unique identifier.sql
@@ -2,3 +2,13 @@
   Date : 2025-12-21
   Description: Leetcode problem-6
 */
+--# Write your MySQL query statement below
+-- “Since all employees must be returned even if a unique ID is missing,
+-- I used a LEFT JOIN from Employees to EmployeeUNI, allowing unmatched rows to return NULL".
+SELECT  
+    e.name , eu.unique_id 
+FROM 
+    Employees AS e
+LEFT JOIN 
+    EmployeeUNI AS eu
+ON e.id = eu.id
