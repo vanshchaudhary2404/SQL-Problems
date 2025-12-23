a11cb7de1dd49041e85af58c1b86cae4d7807897
Vansh Kumar <vanshchaudhary2404@gmail.com>
2025-12-23 12:29:11 +0530

    Update Customer Who Visited but Did Not Make Any Transactions.sql

--- Customer Who Visited but Did Not Make Any Transactions.sql
+++ Customer Who Visited but Did Not Make Any Transactions.sql
@@ -3,6 +3,7 @@
   Description: Leetcode problem-8
 */
 --# Write your MySQL query statement below
+--“I used a LEFT JOIN between Visits and Transactions and filtered rows where transaction is NULL to identify visits without transactions, then grouped by customer to count such visits.”
 SELECT 
     v.customer_id , COUNT(*) AS count_no_trans
 FROM 
