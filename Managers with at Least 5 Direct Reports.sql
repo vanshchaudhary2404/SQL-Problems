0021f12dced7e3b2466a85f179d8f82758ac8a7e
Vansh Kumar <vanshchaudhary2404@gmail.com>
2025-12-25 00:22:39 +0530

    Update Managers with at Least 5 Direct Reports.sql

--- Managers with at Least 5 Direct Reports.sql
+++ Managers with at Least 5 Direct Reports.sql
@@ -2,3 +2,13 @@
   Date : 2025-12-24
   Description: Leetcode problem-13
 */
+# Write your MySQL query statement below
+SELECT e.name
+FROM Employee e
+JOIN Employee r
+    ON e.id = r.managerId
+GROUP BY e.id , e.name
+HAVING COUNT(r.id)>= 5;
+
+-- “i self-join the Employee table to map managers with their direct reports,
+--   group by manager, and use HAVING to filter managers with at least five direct reports”
