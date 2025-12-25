ba301c70d102dee76f806d4c69467070eb91cd83
Vansh Kumar <vanshchaudhary2404@gmail.com>
2025-12-25 23:09:19 +0530

    Add MySQL query for average selling price calculation
    Implemented a MySQL query to calculate the average selling price of products based on units sold and price.

--- Average selling price.sql
+++ Average selling price.sql
@@ -2,3 +2,17 @@
   Date : 2025-12-20
   Description: Leetcode problem-16
 */
+# Write your MySQL query statement below
+SELECT 
+    p.product_id ,
+    ROUND( IFNULL(SUM( us.units * p.price ) / SUM(us.units) , 0 ), 2) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold us
+    ON p.product_id = us.product_id
+    AND us.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
+
+
+-- “I joined sales with price periods using the purchase date, computed total 
+-- revenue as units × price, divided by total units to get a weighted average,
+-- handled no-sale products using LEFT JOIN and IFNULL, and rounded to two decimals.”
