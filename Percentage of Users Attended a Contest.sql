86a5fc6eebc77fc8495e3691e4fe891bd98fa541
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-01-01 13:52:29 +0530

    Implement query for contest attendance percentage
    Add SQL query to calculate percentage of users attended a contest based on registered users.

--- Percentage of Users Attended a Contest.sql
+++ Percentage of Users Attended a Contest.sql
@@ -2,3 +2,15 @@
   Date : 2026-01-01
   Description: Leetcode problem-163
 */
+# Write your MySQL query statement below
+-- Formula percentage = (number of users registered in this contest) ​×100 / total number of users.
+SELECT  
+    r.contest_id,
+    ROUND(
+        COUNT(DISTINCT r.user_id ) *100.0 / COUNT(DISTINCT u.user_id)
+        , 2 
+    ) AS percentage
+From Register r
+CROSS JOIN Users u
+GROUP BY r.contest_id
+ORDER BY  percentage DESC , contest_id ASC; 
