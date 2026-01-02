bc9a3e84afb412e41b8aad6a9c5f4758b56a8d21
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-01-02 21:22:46 +0530

    Update Queries Quality and Percentage.sql

--- Queries Quality and Percentage.sql
+++ Queries Quality and Percentage.sql
@@ -2,3 +2,13 @@
   Date : 2026-01-01
   Description: Leetcode problem-1211
 */
+# Write your MySQL query statement below
+SELECT 
+    query_name ,
+    ROUND( AVG( rating / position ), 2 ) AS quality ,
+    ROUND(
+        SUM( CASE WHEN rating < 3 THEN 1 ELSE 0 END)*100.0 / COUNT(*)
+        ,2
+    ) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
