1a6fb3a061aab8ec6b4986e361986d170c2ddf5f
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-05-29 12:20:48 +0530

    Create The Number of Employees Which Report to Each Employee.sql

---
+++ The Number of Employees Which Report to Each Employee.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+SELECT 
+    E1.employee_id,
+    E1.name,
+    COUNT(E2.employee_id) AS reports_count,
+    ROUND(AVG(E2.age)) AS average_age
+FROM Employees as E1 INNER JOIN Employees AS E2
+ON E1.employee_id = E2.reports_to
+GROUP BY E1.employee_id , E1.name
+ORDER BY E1.employee_id
