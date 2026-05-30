3e7ef57827bbd873a4a551ee6b116145818e5919
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-05-30 09:29:12 +0530

    Create 1789. Primary Department for Each Employee.sql

---
+++ 1789. Primary Department for Each Employee.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT DISTINCT employee_id , department_id
+FROM Employee
+WHERE employee_id IN (
+    SELECT employee_id
+    FROM Employee
+    Group BY employee_id
+    HAVING COUNT(*) = 1
+) OR primary_flag = 'Y';
