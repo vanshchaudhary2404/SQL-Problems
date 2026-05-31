d395033b3cce94a9a373e3128dd28f33ae611bbb
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-05-31 10:51:25 +0530

    Create 1527. Patients With a Condition.sql

---
+++ 1527. Patients With a Condition.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT patient_id , patient_name , conditions
+FROM Patients
+WHERE conditions LIKE ("DIAB1%") OR  conditions LIKE ("% DIAB1%")
+
+
+#pattern
+-- word at start
+-- OR
+-- space + word
